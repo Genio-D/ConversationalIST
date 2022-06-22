76a444c6e07c980f48c256f722cbe9db37a376c9
Genio-D <diogodias001@gmail.com>
2022-06-22 16:20:48 +0100

    can join public chatrooms

--- android/app/src/main/java/pt/ulisboa/tecnico/cmov/conversationalist/JoinChatroomActivity.java
+++ android/app/src/main/java/pt/ulisboa/tecnico/cmov/conversationalist/JoinChatroomActivity.java
@@ -2,12 +2,18 @@ package pt.ulisboa.tecnico.cmov.conversationalist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import pt.ulisboa.tecnico.cmov.conversationalist.data.Data;
+import pt.ulisboa.tecnico.cmov.conversationalist.data.backend.BackendManager;
 
 public class JoinChatroomActivity extends AppCompatActivity {
 
@@ -15,9 +21,18 @@ public class JoinChatroomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_chatroom);
-        var chatrooms = Arrays.asList("1-27", "A4");
+        var chatrooms = new ArrayList<>(BackendManager.getPublicChatrooms().getList());
         var adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, chatrooms);
         ListView chatroomsListView = findViewById(R.id.availableChatroomsListView);
+        chatroomsListView.setOnItemClickListener((parent, view, position, id) -> {
+            var chatId = chatrooms.get(position);
+            Log.i("mytag", "joining " + chatId);
+            BackendManager.joinRoom(Data.getUsername(), chatId);
+            Data.updateJoinedChatrooms();
+            var intent = new Intent(this, ChatroomActivity.class);
+            intent.putExtra("chatId", chatId);
+            startActivity(intent);
+        });
         chatroomsListView.setAdapter(adapter);
     }
 }
